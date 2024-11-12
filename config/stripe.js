4e1f1ec49cf9040630f31bf429f9406288b6885a
Arslan <arslanjs.dev@gmail.com>
2024-11-13 01:24:43 +0500

    Move stripe key to env

--- config/stripe.js
+++ config/stripe.js
@@ -6,7 +6,7 @@
 */
 
 // const stripe = require('stripe')('sk_test_51ONgbWF5gKnU5g5lnGcdblzKJ1P4eTpj7O7uD7o9cmWZ8XwCwlIlPR46dOZmPBJfZAR384XGDYdIsaq7uBkF9Ape00v27kTsLQ')
-const stripe = require('stripe')('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')
 
-
+// TODO replace with new key
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 module.exports = stripe;
